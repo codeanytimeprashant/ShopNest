8fc375c06d79a88f604b953e3811abb46b63319f
ppadw <ppadwal520@gmail.com>
2026-06-28 00:11:54 +0530

    Added index.js in routes,added index.ejs and partial folder (header and footer) in views make some changes in app.js as well

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const path = require('path');
 const ownersRouter = require('./routes/ownersRouter');
 const productsRouter = require('./routes/productsRouter');
 const usersRouter = require('./routes/usersRouter');
+const indexRouter = require('./routes/index');
 
 
 
@@ -21,6 +22,8 @@ app.set("view engine","ejs");
 app.use("/owners",ownersRouter);
 app.use("/products",productsRouter);
 app.use("/users",usersRouter);
+app.use("/",indexRouter);
+
 
 
 app.listen(3000);
