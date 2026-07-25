ffa9a336853d3cf3430e43d1eca9f895ee8a1ef9
ppadw <ppadwal520@gmail.com>
2026-07-25 23:55:15 +0530

    fix: use process.env.MONGODB_URI for production MongoDB connection

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
-const config = require('config')
 const dbgr = require('debug')("development:mongoose");
 
-mongoose
-.connect(`${config.get("MONGODB_URI")}/ShopNest`)
-.then(function(){
-    dbgr("connected");
-})
-.catch(function(err){
-    dbgr(err);
-})
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/ShopNest";
 
+mongoose
+    .connect(MONGODB_URI)
+    .then(function () {
+        dbgr("connected to MongoDB");
+        console.log("MongoDB connected");
+    })
+    .catch(function (err) {
+        dbgr(err);
+        console.error("MongoDB connection error:", err.message);
+    })
 
 
 module.exports = mongoose.connection;
